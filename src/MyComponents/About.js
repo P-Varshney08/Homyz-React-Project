415902c0138056c5bdd80287c32787e8a4e608fb
Priya Varshney <priyavarshney081003@gmail.com>
2023-07-09 01:13:25 +0530

    Create About.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+
+const About = () => {
+    return(
+        <>
+            <h1>About wala page h ye</h1>
+            <div className="gallery">
+                
+            </div>
+        </>
+    );
+}
+export default About;
