046fe5b292e930f2b745ba6ffcf9df357f8322fe
Priya Varshney <priyavarshney081003@gmail.com>
2023-07-09 15:56:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Heading from "./MyComponents/Heading";
+import Front from "./MyComponents/Front"
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <div>{"Priya Varshney"}</div>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      {/* <Header/> */}
+      {/* <Heading /> */}
+      <Front />
+      
+    </>
+  );
+}
+
+export default App;
